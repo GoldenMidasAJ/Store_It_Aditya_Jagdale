2a6ccfd7cf43541086c257332309954fbbb6a565
GoldenMidasAJ <adityakj3000@gmail.com>
2025-02-19 10:37:21 +0530

    first commit

---
+++ app/(root)/[type]/page.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Sort from "@/components/Sort";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
+import Card from "@/components/Card";
+import { getFileTypesParams } from "@/lib/utils";
+
+const Page = async ({ searchParams, params }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || "";
+  const searchText = ((await searchParams)?.query as string) || "";
+  const sort = ((await searchParams)?.sort as string) || "";
+
+  const types = getFileTypesParams(type) as FileType[];
+
+  // Fetch the files based on search params
+  const files = await getFiles({ types, searchText, sort });
+
+  // Calculate the total file size (in MB)
+  const totalSizeInBytes = files.documents.reduce((total, file) => {
+    return total + (file.size || 0); // Assuming `size` is in bytes for each file
+  }, 0);
+
+  // Convert bytes to MB (1 MB = 1048576 bytes)
+  const totalSizeInMB = (totalSizeInBytes / 1048576).toFixed(2); // Convert to MB and keep 2 decimal places
+
+  return (
+    <div className="page-container">
+      <section className="w-full">
+        <h1 className="h1 capitalize">{type}</h1>
+
+        <div className="total-size-section">
+          <p className="body-1">
+            Total: <span className="h5">{totalSizeInMB} MB</span>
+          </p>
+
+          <div className="sort-container">
+            <p className="body-1 hidden text-light-200 sm:block">Sort by:</p>
+
+            <Sort />
+          </div>
+        </div>
+      </section>
+
+      {/* Render the files */}
+      {files.total > 0 ? (
+        <section className="file-list">
+          {files.documents.map((file: Models.Document) => (
+            <Card key={file.$id} file={file} />
+          ))}
+        </section>
+      ) : (
+        <p className="empty-list">No files uploaded</p>
+      )}
+    </div>
+  );
+};
+
+export default Page;
